b3d55d3099e7a19ed5aa24c8eeb368d232cb6fb7
tshan <hts0215@naver.com>
2025-10-14 16:46:08 +0900

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,13 @@
-import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
-export default function ModalScreen() {
+export default function LandingScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
+      <ThemedText type="title">Byul Works</ThemedText>
+      <ThemedText>간단한 랜딩 페이지입니다.</ThemedText>
     </ThemedView>
   );
 }
@@ -22,8 +19,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
 });
+
+
